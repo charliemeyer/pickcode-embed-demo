713482f2239881a4d931cae165216580df50aa29
Charlie Meyer <cmey63@gmail.com>
2023-03-12 09:18:21 -0400

    owner id lesson id

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ function App() {
   const [token, setToken] = useState("");
   const [baseURL, setBaseURL] = useState("http://localhost:5173");
   const [organization, setOrganization] = useState("pickcode-dev");
-  const [projectId, setProjectId] = useState("");
-  const url = `${baseURL}/project/${projectId}?preferencesProfile=hello-world#&organization=${organization}&token=${token}`;
+  const [ownerId, setOwnerId] = useState("");
+  const [lessonId, setLessonId] = useState("");
+
+  const url = `${baseURL}/project/embed?lessonId=${lessonId}&ownerId=${ownerId}?preferencesProfile=hello-world#&organization=${organization}&token=${token}`;
   return (
     <div className="h-full w-full bg-[#5C71F2] flex">
       <div className="flex flex-col gap-2 p-4">
@@ -29,12 +31,21 @@ function App() {
           />
         </div>
         <div>
-          <div className="text-white font-bold">ProjectId</div>
+          <div className="text-white font-bold">OwnerId</div>
           <input
             type="text"
             className="rounded p-1"
-            value={projectId}
-            onChange={(e) => setProjectId(e.currentTarget.value)}
+            value={ownerId}
+            onChange={(e) => setOwnerId(e.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <div className="text-white font-bold">LessonId</div>
+          <input
+            type="text"
+            className="rounded p-1"
+            value={lessonId}
+            onChange={(e) => setLessonId(e.currentTarget.value)}
           />
         </div>
         <div>
